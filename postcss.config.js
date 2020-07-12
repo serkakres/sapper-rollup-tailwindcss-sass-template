f13775a787b8e2a64f2096d01875272309a5f54d
serkakres <serkakres@gmail.com>
2020-07-12 16:05:30 +0300

    [dev] Added autoprefixer & cssnano

--- postcss.config.js
+++ postcss.config.js
@@ -1,5 +1,9 @@
+const prod = process.env.NODE_ENV !== 'development';
+
 module.exports = {
   plugins: [
     require('tailwindcss'),
-  ],
+    prod && require('autoprefixer'),
+    prod && require('cssnano'),
+  ].filter(Boolean),
 };
